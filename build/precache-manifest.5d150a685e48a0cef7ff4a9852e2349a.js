b9f4a990977b9c459a929edb9cc73809a300e221
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-07-13 23:51:42 +0530

    Add files via upload

---
+++ build/precache-manifest.5d150a685e48a0cef7ff4a9852e2349a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ef9a27d3279d8472873db4b5ab2cc59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89608a1d209f52d48c6d",
+    "url": "/static/css/main.34d0993a.chunk.css"
+  },
+  {
+    "revision": "cc22c203b1011dc2fd53",
+    "url": "/static/js/2.56322624.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56322624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89608a1d209f52d48c6d",
+    "url": "/static/js/main.7c7042ee.chunk.js"
+  },
+  {
+    "revision": "a09792300aedfe1060d4",
+    "url": "/static/js/runtime-main.d8921639.js"
+  }
+]);
